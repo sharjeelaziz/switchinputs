a7847b9102f3da027610c294a8d1f29dad72f082
Sharjeel Aziz <sharjeel.aziz@gmail.com>
2023-12-23 21:10:14 -0500

    Update monitor inputs
    Signed-off-by: Sharjeel Aziz <sharjeel.aziz@gmail.com>

--- tolinux.sh
+++ tolinux.sh
@@ -5,6 +5,6 @@
 # Switch Lift Mouse to input 1:
 ./hidapitester --vidpid 046D/C548 --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,0x02,0x0A,0x1C,0x00,0x00,0x00
 # Switch Monitor to display port
-ddcutil --display 1 setvcp 0x60 0x09
+ddcutil --display 1 setvcp 0x60 0x10
 #Switch monitor to display port
-ddcutil --display 2 setvcp 0x60 0x0f
+ddcutil --display 2 setvcp 0x60 0x11
--- tomac.sh
+++ tomac.sh
@@ -4,5 +4,7 @@
 ./hidapitester --vidpid 046D/C548 --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,0x01,0x09,0x1C,0x02,0x00,0x00
 # Switch Lift Mouse to input 1:
 ./hidapitester --vidpid 046D/C548 --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,0x02,0x0A,0x1C,0x02,0x00,0x00
-#Switch monitor to HDMI 1
-ddcutil --display 2 setvcp 0x60 0x11
+#Switch monitor to Display Port
+ddcutil --display 2 setvcp 0x60 0x0f
+# Switch monitor to HDMI
+ddcutil --display 1 setvcp 0x60 0x0f
